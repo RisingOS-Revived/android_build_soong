009fdc7dbab96bc5d9f6e50c1e739566e6d72a76
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-30 16:30:06 +0800

    [RisingOS] Add chipset sysprop

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -123,6 +123,7 @@ def parse_args():
   config["LineageDesc"] = config["BuildDesc"]
   config["LineageDevice"] = config["DeviceName"]
   
+  config["RisingChipset"] = ""
   config["RisingMaintainer"] = ""
 
   if config["BuildNumber"].startswith("eng."):
@@ -252,6 +253,7 @@ def generate_build_info(args):
 
   print(f"ro.lineage.device={config['LineageDevice']}")
   
+  print(f"ro.rising.chipset={config['RisingChipset']}")
   print(f"ro.rising.maintainer={config['RisingMaintainer']}")
   
 
